7fed2dad4dd0c06f3f551e873ae59f906d90dea4
skytree010 <redhun7@gmail.com>
2022-07-14 13:50:21 +0900

    add function -> byte, halfword save and load

--- 16.code_data_memory/data_memory.v
+++ 16.code_data_memory/data_memory.v
@@ -19,6 +19,9 @@ module data_memory
     reg [31:0] data_mem [128:0];
     wire [31:0] addr_comb;
     wire [31:0] addr_mem_comb;
+    wire [31:0] data_mem_word;
+    wire [31:0] data_mem_half;
+    wire [31:0] data_mem_byte;
 
     reg [4:0] write_addr_reg;
     reg [31:0] write_data_reg;
@@ -26,6 +29,9 @@ module data_memory
 
     assign addr_comb = src1_value + imm;
     assign addr_mem_comb = {{2{1'b0}}, addr_comb[31:2]};
+    assign data_mem_word = data_mem[addr_mem_comb];
+    assign data_mem_half = data_mem_word >> addr_comb[1];
+    assign data_mem_byte = data_mem_word >> addr_comb[1:0];
 
     always @(posedge clk, negedge reset_n) begin
         if(!reset_n) begin
@@ -35,31 +41,51 @@ module data_memory
         end else begin
             if(!jump_branch_enable) begin
                 case(operation_con)
-                /*
                     LB: begin
-                        write_data_reg <= data_mem[addr_mem_comb][{addr_mem_pos_comb + 1 , {3{1'b0}}}:{addr_mem_pos_comb, {3{1'b0}}}];
-                    end
-                    LH: begin
-                        write_data_reg <= data_mem[addr_mem_comb][{addr_mem_pos_comb + 2 , {3{1'b0}}}:{addr_mem_pos_comb, {3{1'b0}}}];
-                    end
-                */
-                    LW: begin
-                        write_data_reg <= data_mem[addr_mem_comb];
+                        write_data_reg[7:0] <= data_mem_byte[7:0];
+                        write_data_reg[31:8] <= {24{data_mem_byte[7]}};
                         write_addr_reg <= rd;
                         write_req_reg <= 1'b1;
                     end
-                /*
+                    LH: begin
+                        write_data_reg[15:0] <= data_mem_half[15:0];
+                        write_data_reg[31:16] <= {16{data_mem_half[15]}};
+                        write_addr_reg <= rd;
+                        write_req_reg <= 1'b1;
+                    end
+                    LW: begin
+                        write_data_reg <= data_mem_word;
+                        write_addr_reg <= rd;
+                        write_req_reg <= 1'b1;
+                    end
                     LBU: begin
-                        write_data_reg <= data_mem[addr_mem_comb];
+                        write_data_reg[7:0] <= data_mem_byte[7:0];
+                        write_data_reg[31:8] <= {24{1'b0}};
+                        write_addr_reg <= rd;
+                        write_req_reg <= 1'b1;
                     end
                     LHU: begin
-                        write_data_reg <= data_mem[addr_mem_comb];
+                        write_data_reg[15:0] <= data_mem_half[15:0];
+                        write_data_reg[31:16] <= {16{1'b0}};
+                        write_addr_reg <= rd;
+                        write_req_reg <= 1'b1;
                     end
                     SB: begin
+                        case(addr_comb[1:0])
+                            0: data_mem[addr_mem_comb][7:0] <= src2_value[7:0];
+                            1: data_mem[addr_mem_comb][15:8] <= src2_value[7:0];
+                            2: data_mem[addr_mem_comb][23:16] <= src2_value[7:0];
+                            3: data_mem[addr_mem_comb][31:24] <= src2_value[7:0];
+                        endcase
+                        write_req_reg <= 1'b0;
                     end
                     SH: begin
+                        if(addr_comb[1])
+                            data_mem[addr_mem_comb][31:16] <= src2_value[15:0];
+                        else
+                            data_mem[addr_mem_comb][15:0] <= src2_value[15:0];
+                        write_req_reg <= 1'b0;
                     end
-                */
                     SW: begin
                         data_mem[addr_mem_comb] <= src2_value;
                         write_req_reg <= 1'b0;
